eb04a6b92f613d776d0d6913ca8feb094eaab4fd
Artrlc <97354721+Artrlc@users.noreply.github.com>
2024-04-16 02:36:37 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Sanchit0102/MARATHI_FLTR.git /MARATHI_FLTR
+  git clone https://github.com/Artrlc/FltrBot.git /FltrBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /MARATHI_FLTR
+  git clone $UPSTREAM_REPO /FltrBot
 fi
-cd /MARATHI_FLTR
+cd /FltrBot
 pip3 install -U -r requirements.txt
 echo "Starting Lucifer...."
 python3 bot.py
